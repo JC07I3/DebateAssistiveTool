3f0f5cdcfc421d6c85806f3427124759cc0398c8
JC0713 <chenshen0713@gmail.com>
2025-03-08 23:42:04 +0800

    新增盃賽與持方選項

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import streamlit as st 
 import pandas as pd
 import time
+import Data
 
 st.title("辯論助手")
 
 st.sidebar.write("## 儲存資料")
 
+com_sp, side_sp = st.sidebar.columns(2)
+
+side_options = ["全部", "正方", "反方", "中性"]
+
+comp_name = st.sidebar.selectbox("盃賽", Data.comp_list, index=0)
+
+side_chosen = st.sidebar.selectbox("持方", side_options, index=0)
+
+def add_option(new_option):
+    Data.comp_list.insert(0, str(new_option))
+
+if comp_name == "輸入其他盃賽":
+    enter_comp_option = st.sidebar.text_input("輸入盃賽")
+    if enter_comp_option:
+        if enter_comp_option in Data.comp_list:
+            st.sidebar.warning("盃賽已存在")
+        else:
+            add_option(enter_comp_option)
+            st.rerun()
+            
 if "data_n" not in st.session_state:
     st.session_state["data_n"] = ""
 
@@ -43,3 +64,5 @@ tb = pd.DataFrame(
 )
 
 st.write(tb)
+
+st.divider()
